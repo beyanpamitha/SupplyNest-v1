d45e5f6e336015f9314e90e7cd6b091cab982fd7
beyanpamitha <beyanpamitha@gmail.com>
2025-12-15 11:42:55 +0530

    Initial commit – SupplyNest v1 microservices project

---
+++ payment-service/src/test/java/com/supplynest/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplynest.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
