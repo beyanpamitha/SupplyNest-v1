e7af5710fccfacf84cd180b6f63690ddc4f301b4
beyanpamitha <beyanpamitha@gmail.com>
2025-12-29 16:50:27 +0530

    Implemented api gateway

---
+++ catalog-service/src/main/java/com/supplynest/catalog_service/controller/CatalogController.java
@@ -0,0 +1,9 @@
+package com.supplynest.catalog_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/catalog")
+public class CatalogController {
+}
---
+++ inventory-service/src/main/java/com/supplynest/inventory_service/controller/InventoryController.java
@@ -0,0 +1,9 @@
+package com.supplynest.inventory_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+public class InventoryController {
+}
