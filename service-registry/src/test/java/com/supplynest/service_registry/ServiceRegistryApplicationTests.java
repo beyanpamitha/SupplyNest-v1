90b05bde7a2b9cc504908a399f2886794fbf87a4
beyanpamitha <beyanpamitha@gmail.com>
2025-12-29 15:11:14 +0530

    Implemented service registry

---
+++ service-registry/src/test/java/com/supplynest/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplynest.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
