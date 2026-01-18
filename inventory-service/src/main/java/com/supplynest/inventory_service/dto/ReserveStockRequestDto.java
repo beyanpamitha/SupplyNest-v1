9aa68ce192ec0f8e0b7f86804f9ad2b6dd3480ef
beyanpamitha <beyanpamitha@gmail.com>
2026-01-18 20:21:12 +0530

    Updated reserve method in inventory service

--- inventory-service/src/main/java/com/supplynest/inventory_service/dto/ReserveStockRequestDto.java
+++ inventory-service/src/main/java/com/supplynest/inventory_service/dto/ReserveStockRequestDto.java
@@ -1,7 +1,13 @@
 package com.supplynest.inventory_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ReserveStockRequestDto {
     private Long productId;
     private BigDecimal quantity;
--- inventory-service/src/main/java/com/supplynest/inventory_service/service/impl/ProductStockServiceImpl.java
+++ inventory-service/src/main/java/com/supplynest/inventory_service/service/impl/ProductStockServiceImpl.java
@@ -1,11 +1,15 @@
 package com.supplynest.inventory_service.service.impl;
 
 import com.supplynest.inventory_service.dto.ReserveStockRequestDto;
+import com.supplynest.inventory_service.entity.InventoryLogs;
+import com.supplynest.inventory_service.entity.ProductStock;
+import com.supplynest.inventory_service.entity.enums.ChangeType;
 import com.supplynest.inventory_service.repository.InventoryLogsRepo;
 import com.supplynest.inventory_service.repository.ProductStockRepo;
 import com.supplynest.inventory_service.service.ProductStockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +19,43 @@ public class ProductStockServiceImpl implements ProductStockService {
     private final InventoryLogsRepo inventoryLogsRepo;
     
     @Override
+    @Transactional
     public void reserveStock(ReserveStockRequestDto reserveStockRequestDto) {
 
+        ProductStock stock = productStockRepo.findById(reserveStockRequestDto.getProductId())
+                .orElseThrow(()->
+                        new RuntimeException(
+                                "Stock not found for product " + reserveStockRequestDto.getProductId()
+                        )
+                );
+
+        //Validating the availability of stock
+        if (stock.getAvailableQuantity().compareTo(reserveStockRequestDto.getQuantity()) < 0){   //Here we can't validate this like .getAvailableQuantity < .getQuantity, because it does not support with BigDecimal.
+            throw new RuntimeException(
+                    "Insufficient stock for product " + reserveStockRequestDto.getProductId()
+            );
+        }
+
+        //Reserving the stock
+        stock.setAvailableQuantity(
+                stock.getAvailableQuantity().subtract(reserveStockRequestDto.getQuantity())  //Can't subtract like usual because of BigDecimal
+        );
+
+        stock.setReservedQuantity(
+                stock.getReservedQuantity().add(reserveStockRequestDto.getQuantity())
+        );
+
+        //Saving
+        productStockRepo.save(stock);
+
+        //Audit logs
+        InventoryLogs log = new InventoryLogs();
+
+        log.setProductId(reserveStockRequestDto.getProductId());
+        log.setChangeType(ChangeType.RESERVE);
+        log.setQuantity(reserveStockRequestDto.getQuantity());
+        log.setOrderId(reserveStockRequestDto.getOrderId());
+
+        inventoryLogsRepo.save(log);
     }
 }
